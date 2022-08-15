ef6eaea553c2345b121e23903ad3a8d8e28265e9
saimain-venuslab <saimain.venuslab@gmail.com>
2022-08-15 23:20:53 +0630

    prediction

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-    transpileDependencies: ["vuetify"],
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "BFF",
+    themeColor: "#4c2fe3",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
+    },
+  },
 };
