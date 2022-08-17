fb4711520ef5b19ef74a083c0028f390252774af
saimain-venuslab <saimain.venuslab@gmail.com>
2022-08-17 12:26:43 +0630

    config pwa

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,37 +1,38 @@
 /* eslint-disable no-console */
 
-// import { register } from "register-service-worker";
+import { register } from "register-service-worker";
 
-// if (process.env.NODE_ENV === "production") {
-//     register(`${process.env.BASE_URL}service-worker.js`, {
-//         ready() {
-//             console.log(
-//                 "App is being served from cache by a service worker.\n" +
-//                     "For more details, visit https://goo.gl/AFskqB"
-//             );
-//         },
-//         registered() {
-//             console.log("Service worker has been registered.");
-//         },
-//         cached() {
-//             console.log("Content has been cached for offline use.");
-//         },
-//         updatefound() {
-//             console.log("New content is downloading.");
-//         },
-//         updated() {
-//             console.log("New content is available; please refresh.");
-//         },
-//         offline() {
-//             console.log(
-//                 "No internet connection found. App is running in offline mode."
-//             );
-//         },
-//         error(error) {
-//             console.error("Error during service worker registration:", error);
-//         },
-//     });
-// }
+if (process.env.NODE_ENV === "production") {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready() {
+      console.log(
+        "App is being served from cache by a service worker.\n" +
+          "For more details, visit https://goo.gl/AFskqB"
+      );
+    },
+    registered() {
+      console.log("Service worker has been registered.");
+    },
+    cached() {
+      console.log("Content has been cached for offline use.");
+    },
+    updatefound() {
+      console.log("New content is downloading.");
+    },
+    updated() {
+      console.log("New content is available; please refresh.");
+      window.location.reload(true);
+    },
+    offline() {
+      console.log(
+        "No internet connection found. App is running in offline mode."
+      );
+    },
+    error(error) {
+      console.error("Error during service worker registration:", error);
+    },
+  });
+}
 
 // import { register } from "register-service-worker";
 
@@ -47,21 +48,21 @@
 //   });
 // }
 
-import { Workbox } from "workbox-window";
+// import { Workbox } from "workbox-window";
 
-if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-  const wb = new Workbox("/service-worker.js");
+// if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+//   const wb = new Workbox("/service-worker.js");
 
-  wb.addEventListener("waiting", () => {
-    const result = window.confirm("refresh now?");
-    if (result) {
-      wb.messageSW({ type: "SKIP_WAITING" });
-    }
-  });
+//   wb.addEventListener("waiting", () => {
+//     const result = window.confirm("refresh now?");
+//     if (result) {
+//       wb.messageSW({ type: "SKIP_WAITING" });
+//     }
+//   });
 
-  wb.addEventListener("controlling", () => {
-    window.location.reload();
-  });
+//   wb.addEventListener("controlling", () => {
+//     window.location.reload();
+//   });
 
-  wb.register();
-}
+//   wb.register();
+// }
