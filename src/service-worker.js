257e12757d42a181d7f8d2c94fef7cdaa1392fb3
saimain-venuslab <saimain.venuslab@gmail.com>
2022-08-17 12:47:49 +0630

    config pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,6 @@
-import { Workbox } from "workbox-window";
-
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-Workbox.precaching.suppressWarnings();
-Workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener("message", (event) => {
   if (event.data.action == "SKIP_WAITING") self.skipWaiting();
