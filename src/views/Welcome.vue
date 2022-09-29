2b30d88dfac2f38200c8b55619ccd6bc05402b5d
Sai Main <saimain.venuslab@gmail.com>
2022-09-29 21:35:36 +0630

    welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,19 @@
 <template>
   <v-app id="inspire">
-    <v-main class="grey lighten-3 my-auto">
-      <v-app-bar elevation="0" color="#eeeeee">
-        <v-btn icon @click="$router.go(-1)">
-          <v-icon>mdi-arrow-left-thin</v-icon>
-        </v-btn>
-      </v-app-bar>
+    <v-main class="primary my-auto">
       <v-container fill-height fluid>
         <v-row align="center" justify="center" class="">
           <v-col cols="12">
-            <h3>Welcome</h3>
-            <p>You have registered successfully</p>
-            <router-link to="/tournament">Predict Now</router-link>
+            <h1 class="white--text">Congratulation!</h1>
+            <h3 class="font-weight-medium white--text">
+              You've successfully signed up.
+            </h3>
+            <v-btn
+              block
+              class="mt-5 py-6 primary--text font-weight-medium"
+              to="/tournament"
+              >Predict Now</v-btn
+            >
           </v-col>
         </v-row>
       </v-container>
